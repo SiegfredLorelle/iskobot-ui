7cec92096e8efe67e1908ea21e7cd7c91bb4fb7d
Siegfred Lorelle <siegfredlorelle09@gmail.com>
2025-01-13 22:14:38 +0800

    Style inquiries

--- src/app/(chat)/components/front/SampleInquiry.tsx
+++ src/app/(chat)/components/front/SampleInquiry.tsx
@@ -1,7 +1,9 @@
 export default function SampleInquiry(props: { text: string }) {
   return (
-    <div>
-      {props.text}
-    </div>
+    <button className="flex-shrink-0 w-64 h-64 mx-2 first:ml-0 last:mr-0 rounded-lg bg-primary">
+      <div className="flex items-center justify-center h-full p-6">
+        <p className="text-center text-primary-foreground text-lg">{props.text}</p>
+      </div>
+  </button>
   )
 }
