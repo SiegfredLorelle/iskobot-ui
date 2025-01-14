7282a435ce1791c0f2cce5a8a6b3ff587ca95c29
Siegfred Lorelle <siegfredlorelle09@gmail.com>
2025-01-15 01:39:50 +0800

    Fix controls width

--- src/app/(chat)/components/controls/InputControls.tsx
+++ src/app/(chat)/components/controls/InputControls.tsx
@@ -95,7 +95,7 @@ export default function InputControls() {
   };
 
   return (
-    <div className="h-full w-full bg-primary-clr flex items-center rounded-3xl px-4 py-4 shadow-lg shadow-text-clr/30 mb-4">
+    <div className="fixed inset-x-4 bottom-4 bg-primary-clr flex items-center rounded-3xl px-4 py-4 shadow-lg shadow-text-clr/30">
       {!userInput && !isRecording && (
         <button
           onClick={handleSettings}
