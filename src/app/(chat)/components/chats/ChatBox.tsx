749d71d927bb9c3492faef4efe05a0cfdbc2556c
unknown <angelodepadua2@gmail.com>
2025-01-18 09:42:49 +0800

    eslint

--- src/app/(chat)/components/chats/ChatBox.tsx
+++ src/app/(chat)/components/chats/ChatBox.tsx
@@ -8,7 +8,7 @@ export default function ChatBox({
 }: ChatBoxProps) {
   return (
     <div className={`flex ${isUser ? "flex-row-reverse ml-auto" : "flex-row"}`}>
-      { <ChatBubble isUser={isUser} />} {/* Only show ChatBubble if not isUser */}
+      <ChatBubble isUser={isUser} /> {/* Only show ChatBubble if not isUser */}
       <div
         className={`p-3 bg-primary-clr shadow-md text-text-clr rounded-md ${isUser ? "me-2" : "ms-2"} ${wide ? "p-5 w-full text-2xl font-bold" : "p-4 max-w-md"} `}
       >
--- src/app/(chat)/components/chats/ChatBubble.tsx
+++ src/app/(chat)/components/chats/ChatBubble.tsx
@@ -1,11 +1,13 @@
 export default function ChatBubble({ isUser }: { isUser?: boolean }) {
   return (
     <div className="bg-primary-clr shadow-md rounded-full w-8 h-8 mt-auto shrink-0 overflow-hidden flex items-center justify-center">
-      {!isUser ? <img
-        src="/assets/icons/iskobot-logo.png"
-        alt="Chat Avatar"
-        className="w-full h-full object-cover"
-      /> : null}
+      {!isUser ? (
+        <img
+          src="/assets/icons/iskobot-logo.png"
+          alt="Chat Avatar"
+          className="w-full h-full object-cover"
+        />
+      ) : null}
     </div>
   );
 }
